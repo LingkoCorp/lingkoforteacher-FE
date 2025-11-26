2aecfd43f44c596c78f7f72e1b5ed3ce439137f5
Shawn <jsucheol77@naver.com>
2025-11-26 11:47:31 +0900

    trigger vercel update

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -2,5 +2,5 @@
 export const LEVELS = ["beginner", "intermediate", "advanced"] as const;
 export type Level = typeof LEVELS[number];
 
-// 상태
+// 상태/
 export type WorksheetStatus = "PENDING" | "GENERATING_CONTENT" | "COMPLETED" | "FAILED";
